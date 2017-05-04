e6be8249914642212d5485d71a8e329756a781e8
nnagori <nnagori@uncc.edu>
2017-05-04 19:02:38 -0400

    Initial Commit

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+    public GameObject ballPreFab;
+    public float ballSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject instance = Instantiate(ballPreFab);
+            instance.transform.position = transform.position;
+            Rigidbody rb = instance.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.forward * ballSpeed;
+        }
+    }
+}
